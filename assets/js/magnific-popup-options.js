d16c66a484b99c8b623d6f5e0e130a7e6b81f055
TheErondu <erone007@gmail.com>
2026-05-16 05:56:14 +0100

    fixed bugs

--- assets/js/magnific-popup-options.js
+++ assets/js/magnific-popup-options.js
@@ -19,9 +19,9 @@ $(document).ready(function() {
         // and to which popup will be scaled down
         // By defailt it looks for an image tag:
         opener: function(openerElement) {
-        // openerElement is the element on which popup was initialized, in this case its <a> tag
-        // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-        return openerElement.is('img') ? openerElement : openerElement.find('img');
+          if (openerElement.is('img')) return openerElement;
+          var img = openerElement.find('img');
+          return img.length ? img : openerElement;
         }
       }
     });
